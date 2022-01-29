6a283f2a1eb1b1a1c1b2076add8f5d72113f1871
elidalovisa <elida.arrechea@gmail.com>
2022-01-29 16:33:26 +0100

    add blueprint routes

---
+++ resource-service/routes/api/v1/data-router.js
@@ -0,0 +1,32 @@
+/**
+ * API version 1 routes.
+ *
+ * @author Elida Arrechea
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import { DataController } from '../../../controllers/api/data-controller.js'
+export const router = express.Router()
+const controller = new DataController()
+
+// Provide req.data to the route if :id is present in the route path.
+router.param('id', (req, res, next, id) => controller.loadData(req, res, next, id))
+
+// GET data
+router.get('/', (req, res, next) => controller.findAll(req, res, next))
+
+// POST data
+router.post('/', (req, res, next) => controller.addData(req, res, next))
+
+// GET data/:id
+router.get('/:id', (req, res, next) => controller.find(req, res, next))
+
+// PUT data/:id
+router.put('/:id', (req, res, next) => controller.update(req, res, next))
+
+// PATCH data/:id
+router.patch('/:id', (req, res, next) => controller.updatePartially(req, res, next))
+
+// DELETE data/:id
+router.delete('/:id', (req, res, next) => controller.delete(req, res, next))
