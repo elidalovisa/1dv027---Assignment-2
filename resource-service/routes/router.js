71d72be2e915311c6847f7b579322a80f01529e6
elidalovisa <elida.arrechea@gmail.com>
2022-03-30 11:55:32 -0300

    change env

--- resource-service/routes/router.js
+++ resource-service/routes/router.js
@@ -11,7 +11,8 @@ import { router as v1Router } from './api/v1/router.js'
 
 export const router = express.Router()
 
-router.use('/api/v1', v1Router)
+//router.use('/api/v1', v1Router)
+router.get('/', (req, res) => res.json({ message: 'Hooray! Welcome to version 1 of this very simple RESTful API! (Auth)' }))
 
 // Catch 404 (ALWAYS keep this as the last route).
-router.use('*', (req, res, next) => next(createError(404)))
+//router.use('*', (req, res, next) => next(createError(404)))
