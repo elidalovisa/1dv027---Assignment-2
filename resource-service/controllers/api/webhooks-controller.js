a3c1675a3a47a89919941e6ca51655a636915ce1
elidalovisa <elida.arrechea@gmail.com>
2022-04-06 10:12:11 +0200

    chanage delete hook function

--- resource-service/controllers/api/webhooks-controller.js
+++ resource-service/controllers/api/webhooks-controller.js
@@ -49,7 +49,7 @@ export class WebhooksController {
       res
         .status(201)
         .json({
-          message: 'Hook created. Save your secret key and use it in body: '
+          message: 'Hook created. Save your secret key and use it in body: ',
           key: newHook.id
         }
         )
